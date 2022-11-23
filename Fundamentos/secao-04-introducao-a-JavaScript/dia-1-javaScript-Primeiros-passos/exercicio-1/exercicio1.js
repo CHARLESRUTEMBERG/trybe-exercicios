47337ba518153335e452822430e45c8d80ae2586
Charles <charlesepatricia@hotmail.com>
2022-11-23 18:57:30 -0300

    exercicio1 resolvido

---
+++ Fundamentos/secao-04-introducao-a-JavaScript/dia-1-javaScript-Primeiros-passos/exercicio-1/exercicio1.js
@@ -0,0 +1,16 @@
+const myName = 'Carolina';
+const birthcity = 'Natal';
+let birthYear = 1973;
+
+birthYear = 2030;
+
+/* Quando foi atribuida um novo valor para brirthYear não teve problema porque o let aceita novas atribuições, mas o const não aceita. Nesse caso é uma constante e não uma variável */
+
+birthcity = 'rn'
+
+console.log(myName);
+console.log(birthcity);
+console.log(birthYear);
+
+
+
