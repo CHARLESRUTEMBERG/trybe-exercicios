9262335c5be62c6c6a0c93bccc7cc7d9666cc666
Charles <charlesepatricia@hotmail.com>
2022-11-25 18:24:29 -0300

    Criação do arquivo exercicio1.js e concluído os exercício

---
+++ Fundamentos/secao-04-introducao-a-JavaScript/dia-2-javaScript-Array-e-loopFor/exercicio-1/exercicio1.js
@@ -0,0 +1,14 @@
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let menuServices = menu;
+
+//console.log(menuServices);
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let indexOfPortfolio = menu[2];
+
+//onsole.log(indexOfPortfolio);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+
+console.log(menu);
