299e1e2dfed20aa0e01e4548a734c1ff6a8d25aa
Charles <charlesepatricia@hotmail.com>
2022-11-28 21:36:35 -0300

    Exercício 10 - Identificar um número par.

---
+++ Fundamentos/secao-04-introducao-a-JavaScript/dia-1-javaScript-Primeiros-passos/exercicio-10/exercicio10.js
@@ -0,0 +1,10 @@
+
+const a = 11;
+const b = 31;
+const c = 21;
+
+if (a%2 === 0 || b%2 === 0 || c%2 === 0){
+    console.log(a%2 === 0 || b%2 === 0 || c%2 === 0);
+}else {
+    console.log(a%2 === 0 || b%2 === 0 || c%2 === 0);
+}
