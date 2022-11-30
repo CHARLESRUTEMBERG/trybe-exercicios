643755e3fe361906e59a00e240e21e58a667a56a
Charles <charlesepatricia@hotmail.com>
2022-11-29 22:44:15 -0300

    Exercício2.js - Inverter a posição das letras

---
+++ Fundamentos/secao-04-introducao-a-JavaScript/dia-3-javaScript-Lógica-de-Programação-e-Algoritmos/exercicio-2/exercicio2.js
@@ -0,0 +1,14 @@
+
+let word = 'Trybe';
+let terms = [] ;
+
+for (let index = 0; index <= word.length; index ++) {
+  terms  = word[index].split(" ");
+  console.log(terms);
+  
+
+ // for (let index2 = 0; index2 < terms.length; index2 += 1) {
+ //   if(terms[index2][0] == "#"){
+ //     hashtags.push(terms[index2])
+  //  }     
+  }  console.log(terms);
