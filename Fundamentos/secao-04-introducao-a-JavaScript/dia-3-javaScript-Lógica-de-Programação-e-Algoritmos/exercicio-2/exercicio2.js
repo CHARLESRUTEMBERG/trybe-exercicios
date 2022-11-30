0325fd86dd104c0bf238f34cd7f386e8b0d00e7d
Charles <charlesepatricia@hotmail.com>
2022-11-30 15:37:47 -0300

    Ainda tentaendo resolver o exercício 2 do dia 3 - javaScript

--- Fundamentos/secao-04-introducao-a-JavaScript/dia-3-javaScript-Lógica-de-Programação-e-Algoritmos/exercicio-2/exercicio2.js
+++ Fundamentos/secao-04-introducao-a-JavaScript/dia-3-javaScript-Lógica-de-Programação-e-Algoritmos/exercicio-2/exercicio2.js
@@ -1,14 +1,11 @@
 
-let word = 'Trybe';
-let terms = [] ;
 
-for (let index = 0; index <= word.length; index ++) {
-  terms  = word[index].split(" ");
-  console.log(terms);
-  
+let word = 'tryber';
+let reverseWord = '';
 
- // for (let index2 = 0; index2 < terms.length; index2 += 1) {
- //   if(terms[index2][0] == "#"){
- //     hashtags.push(terms[index2])
-  //  }     
-  }  console.log(terms);
+for (let index = 0; index < word.length; index += 1) {
+  reverseWord += word[word.length - 1 - index];
+  console.log(reverseWord);
+}
+
+console.log(reverseWord);
