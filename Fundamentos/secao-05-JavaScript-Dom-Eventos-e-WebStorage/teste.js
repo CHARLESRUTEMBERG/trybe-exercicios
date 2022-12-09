5cc18de3a4ed05b22247cd7d11b22e0407a157ec
Charles <charlesepatricia@hotmail.com>
2022-12-08 23:09:34 -0300

    Alteração em teste.html e teste.js.

---
+++ Fundamentos/secao-05-JavaScript-Dom-Eventos-e-WebStorage/teste.js
@@ -0,0 +1,10 @@
+
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "blue";
+const subtitle = document.getElementById('subtitle');subtitle.style.color = 'red';
+
+document.getElementById('teste')[1].innerText = 'Maria'
+
+
+document.querySelector('#page-title').style.color = 'red'
+
