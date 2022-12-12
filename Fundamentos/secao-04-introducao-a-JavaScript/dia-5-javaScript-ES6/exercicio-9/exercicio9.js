5238fb626167996249262ba1da6789847987b01e
Charles <charlesepatricia@hotmail.com>
2022-12-12 12:17:43 -0300

    Reorganizando o exercício 9 com o bônus.

--- Fundamentos/secao-04-introducao-a-JavaScript/dia-5-javaScript-ES6/exercicio-9/exercicio9.js
+++ Fundamentos/secao-04-introducao-a-JavaScript/dia-5-javaScript-ES6/exercicio-9/exercicio9.js
@@ -1,12 +1,9 @@
 
 let status = 'desligado';
-const ligaEDesliga = () => {
-    if (status === 'desligado') {
-        return status = 'ligado';
-    } else {
-        return status = 'desligado';
-    }
-}
+const ligaEDesliga = () =>
+    status = status === 'desligado'? 'ligado':'desligado';
+
+
 console.log(`O motor está ${ligaEDesliga()}`);
 console.log(`O motor está ${ligaEDesliga()}`);
 console.log(`O motor está ${ligaEDesliga()}`);
